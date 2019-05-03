80732cdfcfbe1ab5ab320e8f7e3b65ee9aac3b57
Izabela Turaj <i.turaj@gmail.com>
2019-05-03 15:46:06 +0200

    routing for drag-and-drop and form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { ArrivalDepartureComponent } from './arrival-departure/arrival-departure
 import { ArrivalComponent } from './arrival/arrival.component';
 import { DepartureComponent } from './departure/departure.component';
 import { PrintComponent } from './print/print.component';
+import { SecondaryFormComponent } from './secondary-form/secondary-form.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
 
 const routes: Routes = [
   { path: '', component: FlagsComponent },
@@ -17,7 +19,9 @@ const routes: Routes = [
   { path: 'arrival-departure', component: ArrivalDepartureComponent },
   { path: 'arrival', component: ArrivalComponent },
   { path: 'departure', component: DepartureComponent },
-  { path: 'print', component: PrintComponent }
+  { path: 'print', component: PrintComponent },
+  { path: 'drivers-register', component: SecondaryFormComponent },
+  { path: 'workplace-safety', component: DragDropComponent }
 ];
 
 @NgModule({
